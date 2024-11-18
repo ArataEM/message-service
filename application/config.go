1379aadd8ec212a1db181ff71fdb725e3f13339e
Artem Usenko <arata.em@gmail.com>
2024-11-18 14:58:41 +0200

    added config

---
+++ application/config.go
@@ -0,0 +1,33 @@
+package application
+
+import (
+	"log/slog"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	RedisAddress string
+	ServerPort   uint16
+}
+
+func LoadConfig() Config {
+	port, err := strconv.ParseUint(getEnv("SERVER_PORT", "8080"), 10, 16)
+	if err != nil {
+		slog.Error("failed to parse env SERVER_PORT")
+		panic(1)
+	}
+	cfg := Config{
+		RedisAddress: getEnv("REDIS_ADDR", "localhost:6379"),
+		ServerPort:   uint16(port),
+	}
+	return cfg
+}
+
+func getEnv(name, fallback string) string {
+	env, exists := os.LookupEnv(name)
+	if exists {
+		return env
+	}
+	return fallback
+}
