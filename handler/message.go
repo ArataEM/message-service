560ecf8522ead0e7b8ab3c60e7067242ab73948c
Artem Usenko <arata.em@gmail.com>
2024-11-18 13:59:45 +0200

    added handlers implementations

--- handler/message.go
+++ handler/message.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/ArataEM/message-service/model"
 	"github.com/ArataEM/message-service/repository/message"
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
 
@@ -105,13 +107,92 @@ func (m *Message) List(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Message) GetById(w http.ResponseWriter, r *http.Request) {
+	idParam, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		slog.Error("failed uuid parse")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	messageRes, err := m.Repo.Get(r.Context(), idParam)
+	if errors.Is(err, message.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		slog.Error("failed to find by id", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
+	err = json.NewEncoder(w).Encode(messageRes)
+	if err != nil {
+		slog.Error("failed to encode result")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (m *Message) UpdateById(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Text string `json:"text,omitempty"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		slog.Error(fmt.Sprintf("Error decoding: %s", err))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(body.Text)
+	if body.Text == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Field \"text\" cannot be empty"))
+		return
+	}
+	idParam, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		slog.Error("failed uuid parse")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	messageRes, err := m.Repo.Get(r.Context(), idParam)
+	if errors.Is(err, message.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		slog.Error("failed to find by id", "error", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	now := time.Now().UTC()
+	messageRes.UpdatedAt = &now
+	messageRes.Text = body.Text
+
+	err = m.Repo.Update(r.Context(), messageRes)
+	if err != nil {
+		slog.Error("failed to update record", "error", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 }
 
 func (m *Message) DeleteById(w http.ResponseWriter, r *http.Request) {
-
+	idParam, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		slog.Error("failed uuid parse")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = m.Repo.Delete(r.Context(), idParam)
+	if errors.Is(err, message.ErrNotExist) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	} else if err != nil {
+		slog.Error("failed to find by id", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
